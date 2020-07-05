a6c0d3b31b5adf84e2470dc3500a49a09af011a5
Pavel Ponomarev <pavponn@yandex.ru>
2020-07-05 14:06:45 +0300

    Add prop types & style code #1

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,49 +1,63 @@
-import React, { useState } from 'react'
-import { ethers } from "ethers";
-import { usePoller } from "../hooks";
+import React, {useState} from 'react'
+import {ethers} from 'ethers';
+import {usePoller} from '../hooks';
+import PropTypes from 'prop-types'
 
 export default function Balance(props) {
 
-  const [dollarMode, setDollarMode] = useState(true);
-  const [balance, setBalance] = useState();
+    const [dollarMode, setDollarMode] = useState(true);
+    const [balance, setBalance] = useState();
 
 
-  const getBalance = async ()=>{
-    if(props.address && props.provider){
-      try{
-        const newBalance = await props.provider.getBalance(props.address)
-        setBalance(newBalance)
-      }catch(e){
-        console.log(e)
-      }
+    const getBalance = async () => {
+        if (props.address && props.provider) {
+            try {
+                const newBalance = await props.provider.getBalance(props.address)
+                setBalance(newBalance)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    };
+
+    usePoller(() => {
+        getBalance()
+    }, props.pollTime ? props.pollTime : 1999);
+
+    let floatBalance = parseFloat("0.00");
+
+    let usingBalance = balance;
+
+    if (typeof props.balance !== "undefined") {
+        usingBalance = props.balance;
     }
-  }
 
-  usePoller(()=>{getBalance()},props.pollTime?props.pollTime:1999)
+    if (usingBalance) {
+        let etherBalance = ethers.utils.formatEther(usingBalance);
+        parseFloat(etherBalance).toFixed(2);
+        floatBalance = parseFloat(etherBalance)
+    }
 
-  let floatBalance = parseFloat("0.00")
+    let displayBalance = floatBalance.toFixed(4);
 
-  let usingBalance = balance
+    if (props.dollarMultiplier && dollarMode) {
+        displayBalance = "$" + (floatBalance * props.dollarMultiplier).toFixed(2)
+    }
 
-  if(typeof props.balance != "undefined"){
-    usingBalance = props.balance
-  }
-
-  if(usingBalance){
-    let etherBalance = ethers.utils.formatEther(usingBalance)
-    parseFloat(etherBalance).toFixed(2)
-    floatBalance = parseFloat(etherBalance)
-  }
-
-  let displayBalance = floatBalance.toFixed(4)
-
-  if(props.dollarMultiplier && dollarMode){
-    displayBalance = "$"+(floatBalance*props.dollarMultiplier).toFixed(2)
-  }
-
-  return (
-    <span style={{verticalAlign:"middle",fontSize:props.size?props.size:24,padding:8,cursor:'pointer'}} onClick={()=>{setDollarMode(!dollarMode)}}>
+    return (
+        <span style={{verticalAlign: "middle", fontSize: props.size ? props.size : 24, padding: 8, cursor: 'pointer'}}
+              onClick={() => {
+                  setDollarMode(!dollarMode)
+              }}>
       {displayBalance}
     </span>
-  );
+    );
 }
+
+Balance.propTypes = {
+    address: PropTypes.string,
+    provider: PropTypes.object,
+    dollarMultiplier: PropTypes.number,
+    pollTime: PropTypes.number,
+    balance: PropTypes.object
+};
--- packages/react-app/src/components/Header.js
+++ packages/react-app/src/components/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
-import { PageHeader } from 'antd';
+import {PageHeader} from 'antd';
+import PropTypes from 'prop-types';
 
 export default function Header(props) {
-  return (
-    <div onClick={()=>{
-      window.open("https://github.com/austintgriffith/scaffold-eth");
-    }}>
-      <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
-        style={{cursor:'pointer'}}
-      />
-    </div>
-  );
+    return (
+        <div onClick={() => {
+            window.open("https://github.com/austintgriffith/scaffold-eth");
+        }}>
+            <PageHeader
+                title={props.title}
+                subTitle={props.subTitle}
+                style={{cursor: 'pointer'}}
+            />
+        </div>
+    );
 }
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired,
+};
